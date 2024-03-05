d19b724562a82f201c395c557846b7335a980de2
Adam Blahout <blahout.adam@seznam.cz>
2024-03-05 21:34:30 +0100

    initial commit for default skeleton of the website

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen  text-gray-900">
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-orange-200 blur-[10rem] sm:w-[68.75rem] dark:bg-orange-400"></div>
+        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-red-200 blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
