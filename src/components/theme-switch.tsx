f3665d03d15684cc770a26299e7fbf3b475ea451
Adam Blahout <blahout.adam@seznam.cz>
2024-03-15 16:36:27 +0100

    theme switch bg repair

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -5,7 +5,7 @@ export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
   return (
     <button
-      className="fixed bottom-5 right-5 border border-black/10 p-4 rounded-full hover:scale-110 transition-all active:scale-105 shadow-sm"
+      className="fixed bottom-5 right-5 border border-black/10 bg-gray-100  dark:bg-gray-800 dark:border-white/10 p-4 rounded-full hover:scale-110 transition-all active:scale-105 shadow-sm"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
